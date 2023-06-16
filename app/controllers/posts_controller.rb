a36c50a4b2f3560479ba3649fdc1b200a43c96dd
Sumukh Prasad <sumukhprasad@icloud.com>
2023-06-16 21:55:17 +0530

    Added show page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.all
     settingEntry = ActiveAdminSetting.find_by(:name => Rails.application.config_for(:settings)["org_name"][:key])
     @orgname = settingEntry == nil ? Rails.application.config_for(:settings)["org_name"][:value] : settingEntry.string 
+  end
+  def show
+    @post = Post.find(params[:id])
   end
 end
