80cad14e59a3865bc4e930f0130a5c68b56803ab
Sumukh Prasad <sumukhprasad@icloud.com>
2023-06-21 04:45:00 +0530

    Fixed comments bug, added rich text in comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
+  has_rich_text :body
+  validates :body, allow_blank: false, presence: true, length: {maximum: 500, minimum: 1}
+
   belongs_to :post, foreign_key: :post_id, primary_key: :id, required: true
 end
