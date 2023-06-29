7dfe65c9dad8d4806b76b7679664c5668d5a6423
Sumukh Prasad <sumukhprasad@icloud.com>
2023-06-29 21:14:53 +0530

    Added user avatars.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
      def configure_permitted_parameters
           added_attrs = [:name, :username, :email, :password, :password_confirmation, :remember_me]
-          additional_attrs = [:about]
+          additional_attrs = [:about, :avatar]
           devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
           devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
           devise_parameter_sanitizer.permit :account_update, keys: added_attrs + additional_attrs
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
+include ActionView::Helpers::AssetUrlHelper
+
 class UsersController < ApplicationController
      def show
           @user = User.find_by_username(params[:username])
+          @user_avatar_path = @user.avatar.attached? ? url_for(@user.avatar) : asset_path('assets/default_user.png')
      end      
 end
